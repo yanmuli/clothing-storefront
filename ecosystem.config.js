f7aad90782bbf0af7ded9efdf43c0f76e5d0f327
yangmuli <muli.yang@ubtrobot.com>
2026-03-17 18:43:59 +0800

    Initial Commit

---
+++ ecosystem.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apps: [
+    {
+      name: 'medusa-storefront',
+      script: 'npm',
+      args: 'run start',
+      cwd: '/var/www/medusa-app/my-medusa-store-storefront',
+    }
+  ]
+};
