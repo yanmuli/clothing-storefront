79f10acb36b1cb5c3303aa124a75c27dd60076f1
yangmuli <muli.yang@ubtrobot.com>
2026-03-18 15:19:40 +0800

    feat: separate development and production env

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,11 +5,6 @@ module.exports = {
       script: 'npm',
       args: 'run start',
       cwd: '/var/www/medusa-app/storefront',
-      env: {
-		MEDUSA_BACKEND_URL: 'http://43.134.101.198:9000',
-		NEXT_PUBLIC_MEDUSA_PUBLISHABLE_KEY: 'pk_13311297e90785bfac1b6c4dfad1bd893acc57a45a17ee1ed688419939eba89b',
-		NEXT_PUBLIC_BASE_URL: 'http://43.134.101.198:8000'
-	  },
     }
   ]
 };
