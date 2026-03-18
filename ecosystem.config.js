300c0975f52b92c0bb32c0665140d11eeef3076a
yangmuli <muli.yang@ubtrobot.com>
2026-03-18 14:12:37 +0800

    add deploy env variables

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,10 +1,15 @@
 module.exports = {
   apps: [
     {
-      name: 'medusa-storefront',
+      name: 'storefront',
       script: 'npm',
       args: 'run start',
-      cwd: '/var/www/medusa-app/my-medusa-store-storefront',
+      cwd: '/var/www/medusa-app/storefront',
+      env: {
+		MEDUSA_BACKEND_URL: 'http://43.134.101.198:9000',
+		NEXT_PUBLIC_MEDUSA_PUBLISHABLE_KEY: 'pk_13311297e90785bfac1b6c4dfad1bd893acc57a45a17ee1ed688419939eba89b',
+		NEXT_PUBLIC_BASE_URL: 'http://43.134.101.198:8000'
+	  },
     }
   ]
 };
